1869dfeac71c4ad5fc8d27280bcf0d0a57323914
J. Gomez <alambike@gmail.com>
2013-12-16 02:07:47 +0100

    Set right command line tool exit status Created Lambom gem interface
    	modified:   bin/lambom
    	modified:   lib/lambom.rb

--- lib/lambom.rb
+++ lib/lambom.rb
@@ -5,17 +5,37 @@ require "mixlib/authentication/signedheaderauth"
 require 'digest/sha1'
 require 'openssl'
 require 'net/http'
-require "lambom/colorized_strings"
+
+require "lambom/config"
+require "lambom/api"
+require "lambom/converger"
 
 module Lambom
-    $debug = false
+    $debug = true
 
     class << self
-        def build_node(argv)
+        def run(argv)
             puts "DEBUG ENABLED" if $debug
-            puts "args recibidos #{argv.inspect}"
-            #Lambom::Node.new().build
+            puts "args recibidos #{argv.inspect}" if $debug
+            
+            #cargar config
+            conf = Lambom::Config.new.load
+            
+            # sobreescribimos a configuracion ca linea de comandos
+            conf.merge(argv)
+
+            attributes = {}
+            # descargar atributos do servidor (a menos que nos pasen json_file => file.json)
+            if argv.has_key?("json_file")
+                json_attributes = IO.read(argv["json_file"])
+            else
+                json_attributes = Lambom::ApiClient.new(conf).get_server_config
+            end
+
+            # executar converxencia
+            Lambom::Converger.new(conf,json_attributes).run
         end
+
 
         def enable_debug
             $debug = true
